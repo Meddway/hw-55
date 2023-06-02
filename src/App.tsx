8aad6642cae0523bcdde8f62c764337b112fced1
Oleg Medvedev <meddway@gmail.com>
2023-06-02 17:38:21 +0600

    Added a function to calculate the total cost of selected ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Ingredients from "./Ingredients/ingredients";
+import Ingredients from './Ingredients/ingredients';
 
+const App: React.FC = () => {
+  const [totalPrice, setTotalPrice] = useState<number>(0);
 
-const App = () => (
-  <div className="App">
-    <Ingredients></Ingredients>
-    <fieldset>
-      <legend>Burger</legend>
-      <div className="Burger">
-        <div className="BreadTop">
-          <div className="Seeds1"></div>
-          <div className="Seeds2"></div>
+  const updateTotalPrice = (newTotalPrice: number) => {
+    setTotalPrice(newTotalPrice);
+  };
+
+  return (
+    <div className="App">
+      <Ingredients updateTotalPrice={updateTotalPrice} />
+      <fieldset>
+        <legend>Burger</legend>
+        <div className="Burger">
+          <div className="BreadTop">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
+          </div>
+          <div className="Salad"></div>
+          <div className="Cheese"></div>
+          <div className="Meat"></div>
+          <div className="BreadBottom"></div>
+          <div className="Price">Price: {totalPrice}</div>
         </div>
-        <div className="Salad"></div>
-        <div className="Cheese"></div>
-        <div className="Meat"></div>
-        <div className="BreadBottom"></div>
-        <div className="Price">Price:</div>
-      </div>
-    </fieldset>
-  </div>
-);
+      </fieldset>
+    </div>
+  );
+};
 
 export default App;
--- src/Ingredients/ingredients.tsx
+++ src/Ingredients/ingredients.tsx
@@ -1,12 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Meat from '../Assets/Meat.png';
 import Bacon from '../Assets/Bacon.png';
 import Cheese from '../Assets/Cheese.png';
 import Salad from '../Assets/Salad.png';
 
-const Ingredients = () => {
+type IngredientsProps = {
+  updateTotalPrice: (newTotalPrice: number) => void;
+};
 
-  const baseGamburgerPrise:number = 30;
+const Ingredients: React.FC<IngredientsProps> = ({ updateTotalPrice }) => {
 
   type Ingredient = {
     name: string;
@@ -16,41 +18,46 @@ const Ingredients = () => {
     id: string;
   };
 
-
   const INGREDIENTS: Ingredient[] = [
-    {name: 'Meat', price: 80, image: Meat, count:0, id: 'meat'},
-    {name: 'Cheese', price: 50, image: Cheese, count:0, id: 'cheese'},
-    {name: 'Salad', price: 10, image: Salad, count:0, id: 'salad'},
-    {name: 'Bacon', price: 60, image: Bacon, count:0, id: 'bacon'},
-  ];
-
-  const [ingredients, setIngredients] = useState<Ingredient[]>([
     { name: 'Meat', price: 80, image: Meat, count: 0, id: 'meat' },
     { name: 'Cheese', price: 50, image: Cheese, count: 0, id: 'cheese' },
     { name: 'Salad', price: 10, image: Salad, count: 0, id: 'salad' },
     { name: 'Bacon', price: 60, image: Bacon, count: 0, id: 'bacon' },
-  ]);
+  ];
+
+  const [ingredients, setIngredients] = useState<Ingredient[]>(INGREDIENTS);
+  const [totalSum, setTotalSum] = useState<number>(30);
 
   const handleDelete = (id: string) => {
     const updatedIngredients = ingredients.map((ingredient) => {
       if (ingredient.id === id) {
+        setTotalSum(totalSum - ingredient.price * ingredient.count);
         return { ...ingredient, count: 0 };
       }
       return ingredient;
     });
     setIngredients(updatedIngredients);
+    const deletedIngredient = ingredients.find((ingredient) => ingredient.id === id);
+    if (deletedIngredient) {
+      updateTotalPrice(totalSum - deletedIngredient.price * deletedIngredient.count);
+    }
   };
 
   const handleIncrement = (id: string) => {
     const updatedIngredients = ingredients.map((ingredient) => {
       if (ingredient.id === id) {
-        return { ...ingredient, count: ingredient.count + 1 };
+        const newCount = ingredient.count + 1;
+        setTotalSum(totalSum + ingredient.price);
+        return { ...ingredient, count: newCount };
       }
       return ingredient;
     });
     setIngredients(updatedIngredients);
+    const incrementedIngredient = ingredients.find((ingredient) => ingredient.id === id);
+    if (incrementedIngredient) {
+      updateTotalPrice(totalSum + incrementedIngredient.price);
+    }
   };
-
 
   return (
     <fieldset>
